602f1197262c6100586eb0b0a29d32ad970839b1
Kenan7 <kenankzmv@gmail.com>
2020-10-28 16:53:00 +0400

    add base api url and getIp method

--- src/app/services/ipaddress.service.ts
+++ src/app/services/ipaddress.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class IpaddressService {
 
-  constructor() { }
+  baseURL = 'http://localhost:8000';
+
+  constructor(private httpService: HttpClient) { }
+
+
+  getIP(ip: string): Observable<any> {
+    return this.httpService.get(this.baseURL + '/get/' + ip);
+  }
+
+
 }
