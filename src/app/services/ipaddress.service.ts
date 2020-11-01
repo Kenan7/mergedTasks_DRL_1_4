d9a97be3e9eb1f8c322782660a35f8c7b5e8b105
Kenan7 <kenankzmv@gmail.com>
2020-11-01 17:46:57 +0400

    http allow origin

--- src/app/services/ipaddress.service.ts
+++ src/app/services/ipaddress.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
 import {catchError} from 'rxjs/operators';
+
+import { HttpHeaders} from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,18 +13,22 @@ export class IpaddressService {
   // baseURL = 'http://localhost:8000';
   baseURL = 'http://35.246.47.238';
 
+  headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  });
 
   constructor(private httpService: HttpClient) { }
 
   getIP(ip: string): Observable<any> {
-    return this.httpService.get(this.baseURL + '/get/' + ip)
+    return this.httpService.get(this.baseURL + '/get/' + ip, {headers: this.headers})
     .pipe(
       catchError(error => this.errorHandler(error))
     );
   }
 
   createEntry(formData: any): Observable<any> {
-    return this.httpService.post(this.baseURL + '/add', formData)
+    return this.httpService.post(this.baseURL + '/add', formData, {headers: this.headers})
     .pipe(
       catchError(error => this.errorHandler(error))
     );
